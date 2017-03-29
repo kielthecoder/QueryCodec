22e199d6fbac2b209e984bf56443b8dada5fe92a
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2017-03-29 13:31:59 -0400

    initial check in of main program

--- QueryCodec/Program.cs
+++ QueryCodec/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,37 @@ namespace QueryCodec
     {
         static void Main(string[] args)
         {
+            string csvFileName = "test.csv";
+            char delim = ',';
+
+            StreamReader reader;
+            string text;
+            int count;
+
+            if (args.Length > 0)
+                csvFileName = args[0];
+
+            try
+            {
+                reader = new StreamReader(csvFileName);
+                text = reader.ReadLine();
+                count = 0;
+
+                while (text != null)
+                {
+                    count++;
+                    text = reader.ReadLine();
+                }
+
+                Console.WriteLine("\nProcessed {0} records", count);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Press Enter to continue");
+            Console.ReadLine();
         }
     }
 }
